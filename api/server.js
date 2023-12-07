c9c88f7ccbe3f893dace7de074d1d63206d16967
Vikram <vikraman@auroville.org.in>
2023-12-07 11:45:43 +0530

    Connect to MongoDB

---
+++ api/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.use(express.json());
+app.use(cors());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const connection = mongoose.connection;
+connection.once('open',() => {
+  console.log("Connection established successfully")
+})
+
+const Todo = require('./models/todo')
+
+app.get('/todos', async(req, res) => {
+  const todos = await Todo.find();
+
+  res.json(todos);
+})
+
+app.post('/todo/new', (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	})
+
+	todo.save();
+
+	res.json(todo);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+})
